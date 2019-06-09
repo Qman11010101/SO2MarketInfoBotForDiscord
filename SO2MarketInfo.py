3624fefb226f7761b01dabbe0f8bdbe27c737433
Kjuman Enobikto <gekyakibo@usako.net>
2019-06-09 22:30:23 +0900

    変更の取り消し

--- SO2MarketInfo.py
+++ SO2MarketInfo.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 
     SOLD OUT 2 Market Information Bot for Discord
-
     @author: Kjuman Enobikto & YuzuRyo61
     @license: MIT License
-
+    
 """
 
 import configparser
@@ -15,6 +16,6 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 if __name__ == "__main__":
-    # 実行
+    # Run
     dcCli = Client()
     dcCli.run(config['discord']['token'])
