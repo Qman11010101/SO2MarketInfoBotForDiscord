8bcce9cd0887a59ecc4bbe9b6f19f90a9734b38f
Kjuman Enobikto <gekyakibo@usako.net>
2019-06-09 23:44:35 +0900

    JSON読み込み部まで書いた/dataフォルダにテスト用データ追加した(あとで消す)

--- SO2MarketInfo.py
+++ SO2MarketInfo.py
@@ -12,9 +12,25 @@ import configparser
 import json
 import urllib.request as urlreq
 
-from SO2MI import Client #トークンの書かれたconfigの読み込み
+#トークンの書かれたconfigの読み込み
+from SO2MI import Client
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+#販売品取得
+url = "https://so2-api.mutoys.com/json/sale/all.json"
+urlreq.urlretrieve(url, 'data/sale.json')
+
+#注文品取得
+url = "https://so2-api.mutoys.com/json/request/all.json"
+urlreq.urlretrieve(url, 'data/requ.json')
+
+#JSONの読み込み
+s = open("data/sale.json", "r", encoding="utf-8_sig")
+r = open("data/requ.json", "r", encoding="utf-8_sig")
+sale = json.load(s)
+requ = json.load(r)
+
 
 if __name__ == "__main__":
     # Run
