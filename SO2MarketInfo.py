89b0708467d85bea075aa5081bf6e2c8511ed3e8
Kjuman Enobikto <gekyakibo@usako.net>
2019-06-11 00:06:24 +0900

    誤記を訂正
    requ→req

--- SO2MarketInfo.py
+++ SO2MarketInfo.py
@@ -22,7 +22,7 @@ urlreq.urlretrieve(url, 'data/sale.json')
 
 #注文品取得
 url = "https://so2-api.mutoys.com/json/request/all.json"
-urlreq.urlretrieve(url, 'data/requ.json')
+urlreq.urlretrieve(url, 'data/req.json')
 
 #JSONの読み込み
 s = open("data/sale.json", "r", encoding="utf-8_sig")
