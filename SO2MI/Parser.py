e26773683d5d2b74f953e7f98bb02a12b8572d37
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-06-13 18:45:35 +0900

    Implemented

--- SO2MI/Parser.py
+++ SO2MI/Parser.py
@@ -1,27 +1,105 @@
+import datetime
+
 from .Alias import alias
 from .getApi import getApi
 
 def ItemParser(itemName):
+    now = datetime.datetime.now()
+    # API取得部
     item = getApi('item', "https://so2-api.mutoys.com/master/item.json")
     recipe = getApi('recipe', "https://so2-api.mutoys.com/json/master/recipe_item.json")
+    sale = getApi('sale', "https://so2-api.mutoys.com/json/sale/all.json")
+    req = getApi('req', "https://so2-api.mutoys.com/json/request/all.json")
 
     # 略称などの変換
     itemName = alias(itemName)
     # アイテムID取得部
     itemId = 0
+    itemScaleName = ""
     for col in item:
         if item[str(col)]["name"] == itemName:
             itemId = col
+            itemScaleName = item[str(col)]["scale"]
             break
     
     if int(itemId) == 0:
         for col in recipe:
-            if recipe[col]["name"] == itemName:
+            if recipe[str(col)]["name"] == itemName:
                 itemId = col
+                itemScaleName = item[str(col)]["scale"]
                 break
         if int(itemId) == 0:
             # 見つからない扱い
             return False
     
-    # 此処から先をこれから
-    return "# TODO 返ってきた情報を基にごにょごにょと（途中）"
+    priceSaleArray = []
+    unitSaleArray = []
+    for store in range(len(sale)):
+        if sale[store]["item_id"] == itemId:
+            priceSaleArray.append(sale[store]["price"])
+            unitSaleArray.append(sale[store]["unit"])
+    priceSaleArray.sort() # 金額ソート
+
+    priceReqArray = []
+    unitReqArray = []
+    for store in range(len(req)):
+        if req[store]["item_id"] == itemId:
+            priceReqArray.append(req[store]["price"])
+            unitReqArray.append(req[store]["buy_unit"])
+    priceReqArray.sort(reverse=True)
+
+    if priceSaleArray != []:
+        saleCheapest = priceSaleArray[0] # 最安値
+        saleMostExpensive = priceSaleArray[-1] # 高額値
+
+        if len(priceSaleArray) < 5:
+            saleMarketPrice = sum(priceSaleArray) // len(priceSaleArray)
+        else:
+            saleMarketPrice = (priceSaleArray[0] + priceSaleArray[1] + priceSaleArray[2] + priceSaleArray[3] + priceSaleArray[4]) // 5
+        
+        saleAverage = sum(priceSaleArray) // len(priceSaleArray)
+        saleUnitSum = sum(unitSaleArray)
+
+        saleStr = f"""最安値: `{str(saleCheapest)}G`
+        最高額値: `{str(saleMostExpensive)}G`
+        最安TOP5平均: `{str(saleMarketPrice)}G`
+        全体平均: `{str(saleAverage)}G`
+        市場全体の個数: `{str(saleUnitSum)}{itemScaleName}`
+        """
+    else:
+        saleStr = "*現在販売されていません。*"
+    
+    if priceReqArray != []:
+        reqMostExpensive = priceReqArray[0] # 最高値
+        reqCheapest = priceReqArray[-1] # 最安値
+
+        if len(priceReqArray) < 5:
+            reqMarketPrice = sum(priceReqArray) // len(priceReqArray)
+        else:
+            reqMarketPrice = (priceReqArray[0] + priceReqArray[1] + priceReqArray[2] + priceReqArray[3] + priceReqArray[4]) // 5
+        
+        reqAverage = sum(priceReqArray) // len(priceReqArray)
+        reqUnitSum = sum(unitReqArray)
+
+        reqStr = f"""最高額値: `{str(reqMostExpensive)}G`
+        最安値: `{str(reqCheapest)}G`
+        最高TOP5平均: `{str(reqMarketPrice)}G`
+        全体平均: `{str(reqAverage)}G`
+        市場全体の注文数: `{str(reqUnitSum)}{itemScaleName}`
+        """
+    else:
+        reqStr = "*現在注文はありません。*"
+
+    # まとめ
+    parsedTime = now.strftime('%Y年%m月%d日%H時')
+    summary = f"""{parsedTime}現在の{itemName}の状況は以下のとおりです。
+
+    **販売：**
+    {saleStr}
+
+    **注文：**
+    {reqStr}
+
+    **時間経過により市場がこの通りでない可能性があります。**
+    """
+    return summary
