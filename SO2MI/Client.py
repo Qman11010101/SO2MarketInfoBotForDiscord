de24698dbc28d8630f56824c1d47c64ef4c9855c
Kjuman Enobikto <gekyakibo@usako.net>
2019-07-07 18:15:05 +0900

    バージョンあげ

--- SO2MI/Client.py
+++ SO2MI/Client.py
@@ -223,7 +223,7 @@ class Client(discord.Client):
             verMsg = textwrap.dedent("""
             **SOLD OUT 2 市場情報bot for Discord**
 
-            Version 2.3
+            Version 2.3.1
             製作者: キューマン・エノビクト、ゆずりょー
             ライセンス: MIT License
             リポジトリ: https://github.com/Qman11010101/SO2MarketInfoBotForDiscord
