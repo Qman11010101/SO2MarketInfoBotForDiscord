2e46d3a41b862809c017e2cbaca710d4f3eaf304
Kjuman Enobikto <gekyakibo@usako.net>
2019-10-11 21:30:54 +0900

    情報をより詳細にした
    まとめ売りとばら売りで情報を分けたり百分率表示したりした

--- SO2MI/Shelf.py
+++ SO2MI/Shelf.py
@@ -22,12 +22,29 @@ def getShelves(townName):
         raise NoTownError("such town does not exists")
 
     # 棚数・販売額取得部
-    shelf = 0
-    sumPrice = 0
+    sumShelf = sumShelfBundle = sumShelfNotBundle = 0
+    sumPrice = sumPriceBundle = sumPriceNotBundle = 0
+    shelfBundlePercent = shelfNotBundlePersent = 0
+    priceBundlePercent = priceNotBundlePercent = 0
     for col in range(len(sale)):
         if sale[col]["area_id"] == townId:
-            shelf += 1
+            sumShelf += 1
             sumPrice += int(sale[col]["price"] * sale[col]["unit"])
+            if sale[col]["bundle_sale"]:
+                sumShelfBundle += 1
+                sumPriceBundle += int(sale[col]["price"] * sale[col]["unit"])
+
+    # 非まとめ売り算出
+    sumShelfNotBundle = sumShelf - sumShelfBundle
+    sumPriceNotBundle = sumPrice - sumPriceBundle
+
+    # まとめ売り百分率
+    shelfBundlePercent = float("{:.2f}".format((sumShelfBundle / sumShelf) * 100))
+    priceBundlePercent = float("{:.2f}".format((sumPriceBundle / sumPrice) * 100))
+
+    # 非まとめ売り百分率
+    shelfNotBundlePersent = 100 - shelfBundlePercent
+    priceNotBundlePercent = 100 - priceBundlePercent
 
     # 時刻をsale-*.jsonから推測
     target = glob.glob("api-log/sale-*.json")
@@ -37,7 +54,12 @@ def getShelves(townName):
     {jsonTime.strftime("%Y{0}%m{1}%d{2} %H{3}%M{4}").format("年", "月", "日", "時", "分")}現在の{townName}の合計販売額・販売棚数は以下の通りです。
 
     合計販売額: {str("{:,}".format(sumPrice))}G
-    販売棚数: {str("{:,}".format(shelf))}個
+    　まとめ売り: {str("{:,}".format(sumPriceBundle))}G ({priceBundlePercent}%)
+    　ばら売り: {str("{:,}".format(sumPriceNotBundle))}G ({priceNotBundlePercent}%)
+
+    販売棚数: {str("{:,}".format(sumShelf))}個
+    　まとめ売り: {str("{:,}".format(sumShelfBundle))}個 ({shelfBundlePercent}%)
+    　ばら売り: {str("{:,}".format(sumShelfNotBundle))}個 ({shelfNotBundlePersent}%)
     """)
 
     return retStr
